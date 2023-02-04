1d7e04eb0d129a7dcc6562181dc51cef13657b2a
ixplodd <egorsgor01@gmail.com>
2023-02-04 18:27:40 +0300

    t1

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Enter number");
+string? strnumber = Console.ReadLine();
+int number = Convert.ToInt32(strnumber);
+
+int numberA = number / 10;
+int numberB = numberA %10;
+System.Console.WriteLine(numberB);
