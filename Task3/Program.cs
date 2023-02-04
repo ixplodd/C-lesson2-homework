3913d4281589b083d393a97d2ab0b86320544427
ixplodd <egorsgor01@gmail.com>
2023-02-04 18:28:01 +0300

    t3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Enter number");
+string strnumber = Console.ReadLine();
+int number = Convert.ToInt32(strnumber);
+
+if ( number == 6) 
+{
+    System.Console.WriteLine("Yes");
+}
+else if ( number == 7) 
+{
+    System.Console.WriteLine("Yes");
+}
+else if (number > 7)
+{
+    System.Console.WriteLine("There is no day of week");
+}
+else
+System.Console.WriteLine("No");
